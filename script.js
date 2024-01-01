b36293bdffc1771bd254de20d8ef181d2dfaf994
tmk-st <tomoka.sato.813@gmail.com>
2024-01-01 19:22:59 +0900

    JSのセミコロン削除

--- script.js
+++ script.js
@@ -1,74 +1,74 @@
-// クリック時のスクロールをスムーズにする
+// メニュークリック時のスクロールをスムーズにする
 document.addEventListener('click', function(event) {
     if (event.target.matches('a[href^="#"]')) {
-      event.preventDefault();
-      const target = document.querySelector(event.target.getAttribute('href'));
-      target.scrollIntoView({ behavior: 'smooth' });
+      event.preventDefault()
+      const target = document.querySelector(event.target.getAttribute('href'))
+      target.scrollIntoView({ behavior: 'smooth' })
     }
-  });
+  })
   
 
 // 日本語・英語切り替え
-const enBtn = document.getElementById('en_btn');
-const jpBtn = document.getElementById('jp_btn');
-const en = document.getElementsByClassName('en');
-const jp = document.getElementsByClassName('jp');
+const enBtn = document.getElementById('en_btn')
+const jpBtn = document.getElementById('jp_btn')
+const en = document.getElementsByClassName('en')
+const jp = document.getElementsByClassName('jp')
 
 const toggleDisplay = (elements, displayValue) => {
   Array.from(elements).forEach(element => {
-    element.style.display = displayValue;
-  });
-};
+    element.style.display = displayValue
+  })
+}
 
-toggleDisplay(en, 'none');
+toggleDisplay(en, 'none')
 
 jpBtn.addEventListener('click', function() {
-  toggleDisplay(en, 'none');
-  toggleDisplay(jp, 'block');
-});
+  toggleDisplay(en, 'none')
+  toggleDisplay(jp, 'block')
+})
 
 enBtn.addEventListener('click', function() {
-  toggleDisplay(jp, 'none');
-  toggleDisplay(en, 'block');
-});
+  toggleDisplay(jp, 'none')
+  toggleDisplay(en, 'block')
+})
 
 // ボタンを画面上に固定する共通処理
 function handleScroll(element, offsetTop, position, top, side) {
-  var scrollTop = document.documentElement.scrollTop;
+  var scrollTop = document.documentElement.scrollTop
 
   if (scrollTop >= offsetTop - 50) {
-    element.style.position = position;
-    element.style.top = top;
-    element.style[side] = windowWidth >= 520 ? '5rem' : '2rem';
+    element.style.position = position
+    element.style.top = top
+    element.style[side] = windowWidth >= 520 ? '5rem' : '2rem'
   } else {
-    element.style.position = 'static';
+    element.style.position = 'static'
   }
 }
 
-const windowWidth = window.innerWidth;
+const windowWidth = window.innerWidth
 
 // 日本語・英語切り替えボタン
-var langBtnElements = document.getElementsByClassName('lang_btn');
-var langBtn = langBtnElements[0];
-var langBtnOffsetTop = langBtn.offsetTop;
+var langBtnElements = document.getElementsByClassName('lang_btn')
+var langBtn = langBtnElements[0]
+var langBtnOffsetTop = langBtn.offsetTop
 
 window.addEventListener('scroll', function() {
-  handleScroll(langBtn, langBtnOffsetTop, 'fixed', '2rem', 'right');
-});
+  handleScroll(langBtn, langBtnOffsetTop, 'fixed', '2rem', 'right')
+})
 
 // プロフページ内戻るボタン
-var backBtn = document.getElementById('back_arrow');
-var backBtnOffsetTop = backBtn.offsetTop;
+var backBtn = document.getElementById('back_arrow')
+var backBtnOffsetTop = backBtn.offsetTop
 
 window.addEventListener('scroll', function() {
-  handleScroll(backBtn, backBtnOffsetTop, 'fixed', '2rem', 'left');
+  handleScroll(backBtn, backBtnOffsetTop, 'fixed', '2rem', 'left')
   if (document.documentElement.scrollTop < langBtnOffsetTop - 50) {
-    backBtn.style.position = 'absolute';
-    backBtn.style.top = '6rem';
+    backBtn.style.position = 'absolute'
+    backBtn.style.top = '6rem'
   }
-});
+})
 
 // ウィンドウ幅を取得
 function getWindowWidth() {
-  return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth || 0;
+  return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth || 0
 }
